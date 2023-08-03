f22094f3729d232c9532af8e1adc1341b294fe3b
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-08-03 17:57:45 +0900

    user auth - in memory

--- security-java/src/main/java/me/janek/securityjava/config/BasicAuthSecurityConfiguration.java
+++ security-java/src/main/java/me/janek/securityjava/config/BasicAuthSecurityConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -34,6 +37,21 @@ public class BasicAuthSecurityConfiguration {
                     .allowedOrigins("http://localhost:3000");
             }
         };
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("janek")
+            .password("{noop}1234")
+            .roles("USER")
+            .build();
+
+        var admin = User.withUsername("admin")
+            .password("{noop}1234")
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
