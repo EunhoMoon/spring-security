667b0f6cf0818a695018465ffcb4345cbd6f0da0
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-08-03 14:33:05 +0900

    java project init

---
+++ security-java/src/test/java/me/janek/securityjava/SecurityJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.janek.securityjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
